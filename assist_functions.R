ee25c73b9869221725a0f1cfe23f5bc490edc831
Aaron <foss.aaron@gmail.com>
2015-11-20 00:18:27 -0600

    feat: add boxoffice mojo individual movie data scraping

--- assist_functions.R
+++ assist_functions.R
@@ -441,7 +441,6 @@ grab_mojo_toc <- function(){
   return(letter_list)
 }
 
-
 #Extracts movie pages from BoxOffice Mojo hrefs
 extract_mojo_movie_pages <- function(movie_list){
   elements <- NULL
@@ -452,7 +451,6 @@ extract_mojo_movie_pages <- function(movie_list){
   }
   return(elements)
 }
-
 
 ##grabs BoxOffice Mojo List of links on alphabetical pages 
 grab_mojo_movies_links <- function(letter){
@@ -472,16 +470,288 @@ grab_mojo_movies_links <- function(letter){
   sub_pages <- getHTMLLinks(dl_url, xpQuery = 
                               "//*[@class = 'alpha-nav-holder']//a/@href")
   sub_pages <- unique(sub_pages)
-  movie_list <- getHTMLLinks(dl_url, xpQuery = "//table//tr//td//a/@href")
-  movie_links <- extract_mojo_movie_pages(movie_list)
+ 
+  movie_list <- getHTMLLinks(dl_url,xpQuery = "//table//tr//td//font/a/@href")
+  movie_links <- build_movie_list(movie_list)
   
   for(pg in sub_pages){
     dl_url <- getURL(paste(base_url,pg,sep = ''))
     movie_list <- getHTMLLinks(dl_url, xpQuery = "//table//tr//td//a/@href")
-    movie_links <- c(movie_links,extract_mojo_movie_pages(movie_list))
+    movie_links <- rbind(movie_links,build_movie_list(movie_list))
+    
   }
+  
   
   return(movie_links)
 }
 
+#takes mojo link list and builds movie & date dataframe
+build_movie_list <- function(movie_list){
+  library(data.table)
+  df <- NULL
+  df$filmid <- NULL
+  df$open_date <- NULL
+  id = 0
+  
+  for(element in movie_list){
+    if(element %like% 'id=' & id == 0){
+      df$filmid <- c(df$filmid, element)
+      id = 1
+    } else {
+      if(element %like% 'id=' & id == 1){
+        df$open_date <- c(df$open_date, NA)
+        df$filmid <- c(df$filmid, element)
+        id = 1
+      } else {
+        if(element %like% 'date=' & id == 1){
+          dt <- unlist(strsplit(element,'date='))[2]
+          dt <- substr(dt,1,10)
+          df$open_date <- c(df$open_date,dt)
+          id = 0
+        } else {
+          if(element %like% 'date=' & id == 0){
+            dt <- unlist(strsplit(element,'date='))[2]
+            dt <- substr(dt,1,10)
+            df$open_date <- c(df$open_date,dt)
+            df$filmid <- c(df$filmid,NA)
+            id = 1
+          }
+        }
+      }
+    }
+  }
+  return(as.data.table(df))
+}
+
+##grabs BoxOffice Mojo data from individual movie title page
+grab_mojo_movies_data <- function(filmid){
+  library(rvest)
+  library(XML)
+  library(RCurl)
+  library(rjson)
+  #unload Hmisc package if loaded due to issues
+  if(sum(search()=='package:Hmisc')>0){
+    detach("package:Hmisc", unload=TRUE)	
+  }
+  
+  base_url <- 'http://www.boxofficemojo.com/'
+  dl_url <- getURL(paste(base_url,filmid,'&p=.htm', sep = ""))
+  center_box <- read_html(dl_url)%>%html_nodes('center td')
+  title <- read_html(dl_url)%>%html_nodes('br+ font b')%>%
+    html_text()
+  
+  json_box <- list()
+  json_box$title <- title
+  for(line in center_box){
+    new_line <- try(strsplit(html_text(line,trim = TRUE),': ')[[1]],
+                    silent = TRUE)
+    list_name <- tolower(gsub(' ','_',new_line[1]))
+    value <- new_line[2]
+    json_box[list_name] <- value
+  }
+  
+  b_tags <- xpathSApply(htmlParse(dl_url),"//b",xmlValue)
+  
+  td_tags <- xpathSApply(htmlParse(gsub('<br>','\n',dl_url)),"//td",xmlValue) 
+  
+  json_box <- get_box_from_b_tags(b_tags,json_box)
+  json_box <- get_talent_from_td_tags(td_tags,json_box)
+  
+  sub_pages <- getHTMLLinks(dl_url, xpQuery = 
+                              "//ul/li/a/@href")
+  
+  weekly_url <- paste("/movies/?page=weekly&id=",
+                  unlist(strsplit(filmid,'id='))[2],sep = '')
+  
+  weekend_url <- paste("/movies/?page=weekend&id=",
+                  unlist(strsplit(filmid,'id='))[2],sep = '')
+  
+  weekly <- NULL
+  weekend <- NULL
+  
+  if(length(which(sub_pages == weekly_url))!=0) {
+    dl_weekly <- getURL(paste(base_url,weekly_url,sep = ''))
+    
+    weekly <- try(build_historic_performance(dl_weekly),silent = TRUE)
+    
+    dl_weekend <- getURL(paste(base_url,weekend_url,sep = ''))
+    weekend <- try(build_historic_performance(dl_weekend),silent = TRUE) 
+    
+  }
+  
+  if(class(weekly)!='try-error'){
+    json_box$weekly <- weekly
+  }
+  
+  if(class(weekend)!='try-error'){
+    json_box$weekend <- weekend
+  }
+  
+  json_box$domestic_total_gross <- NULL
+  json_box$release_date <- as.Date(json_box$release_date,'%B %d, %Y')
+  json_box$runtime <- as.chron.ITime(paste(gsub('hrs.|min.',
+                              ':',json_box$runtime),'00',sep = ''))
+  json_box$runtime <- as.numeric(json_box$runtime)*24*60
+  if(json_box$production_budget %like% 'million'){
+    json_box$production_budget <- 
+      as.numeric(gsub('[[:alpha:]]|[[:punct:]]| ','',
+           json_box$production_budget))*1000000
+  } else {
+    json_box$production_budget <- 
+      as.numeric(gsub('[[:alpha:]]|[[:punct:]]| ','',
+           json_box$production_budget))
+  }
+  
+  return(toJSON(json_box,method = 'C'))
+}
+
+#parses boxoffice info from mojo page
+get_box_from_b_tags <- function(b_tags,json_box){
+  json_box$box_office$domestic <- 
+    as.numeric(gsub('\\$|,',"",b_tags[which(b_tags == 'Domestic:')+1]))
+  
+  json_box$box_office$total <- 
+    as.numeric(gsub('\\$|,',"",b_tags[which(b_tags == 'Worldwide:')+1]))
+  
+  json_box$box_office$foreign <- 
+    json_box$box_office$total - json_box$box_office$domestic
+  
+  if(length(json_box$box_office$domestic)==0){
+    json_box$box_office$domestic <- NA
+  }
+  
+  if(length(json_box$box_office$total)==0){
+    json_box$box_office$total <- NA
+  }
+  
+  if(length(json_box$box_office$foreign)==0){
+    json_box$box_office$foreign <- NA
+  }
+  
+  return(json_box)
+}
+
+#parses talent info from mojo page
+get_talent_from_td_tags <- function(td_tags,json_box){
+  json_box$talent$director <- 
+    unlist(strsplit(td_tags[which(td_tags == 'Director:')+1],'\n'))
+  
+  #json_box$talent$writer <- 
+  #  unlist(strsplit(td_tags[max(c(which(td_tags == 'Writer:')+1,
+  #                            which(td_tags == 'Writers:')+1),na.rm=T)],'\n'))
+  
+  json_box$talent$actors <- 
+    gsub('\\*','',unlist(strsplit(td_tags[which(td_tags == 'Actors:')+1],'\n')))
+  
+  if(length(json_box$talent$director)==0){
+    json_box$talent$director <- NA
+  }
+  
+  #if(length(json_box$talent$writer)==0){
+  #  json_box$talent$writer <- NA
+  #}
+  
+  if(length(json_box$talent$actors)==0){
+    json_box$talent$actors <- NA
+  }
+  
+  return(json_box)
+}
+
+#build table of historic performance from mojo data
+build_historic_performance <- function(dl_file){
+  library(XML)
+  library(RCurl)
+  library(data.table)
+  library(rvest)
+  
+  weekend_links <- NULL
+  weekend_days <- NULL
+  weekend_grosses <- NULL
+  weekend_theaters <- NULL
+  weekend_release <- NULL
+  
+  weekend_links <- getHTMLLinks(dl_file, xpQuery ="//font/a/@href")
+  weekend_links<- weekend_links[which(weekend_links %like% 'yr=')]
+  
+  weekend_days_column <- 'nobr font a'
+  weekend_days <- read_html(dl_file)%>%html_nodes(weekend_days_column)%>%
+    html_text()
+  weekend_days <- na.omit(gsub('Â–','-',weekend_days))
+  
+  weekend_gross_column <- 'tr+ tr td:nth-child(3) font'
+  weekend_grosses <- read_html(dl_file)%>%html_nodes(weekend_gross_column)%>%
+    html_text()
+  weekend_grosses <- na.omit(as.numeric(gsub('[[:punct:]]|[[:alpha:]]','',weekend_grosses[-1])))
+  
+  weekend_theater_column <- 'tr+ tr td:nth-child(5) font'
+  weekend_theaters <- read_html(dl_file)%>%html_nodes(weekend_theater_column)%>%
+    html_text()
+  weekend_theaters <- na.omit(as.numeric(gsub('[[:punct:]]','',weekend_theaters)))
+  
+  weekend_release_column <- 'tr+ tr td:nth-child(9) font'
+  weekend_release <- read_html(dl_file)%>%html_nodes(weekend_release_column)%>%
+    html_text()
+  weekend_release <- na.omit(as.numeric(gsub('[[:punct:]]','',weekend_release)))
+  weekend_release <- c(1:length(weekend_release))
+  
+  out <- lapply(c(1:length(weekend_release)),
+                build_week_performance,weekend_release,weekend_links,weekend_days,
+                weekend_grosses,weekend_theaters)
+  out <- rbindlist(out)
+  
+  #fixes bad handling of 3 day weekends
+  if(median(out[[4]])<5){
+    remove <- which(out[,days]>2)-1
+    out <- out[!remove]
+    out[,rel_week:=c(1:dim(out)[1])]
+  }
+  
+  return(out)
+}
+
+#build a single week's performance
+build_week_performance <- function(i,rel,lnks,dys,gro,thea){
+  library(data.table)
+  
+  rel_week <- rel[i]
+  norm_date <- unlist(strsplit(lnks[i],'='))
+  year <- substr(norm_date[2],1,4)
+  week <- substr(norm_date[3],1,2)
+  dates <- unlist(strsplit(dys[i],'-'))
+  if(!is.na(as.numeric(dates[2]))){
+    end <- paste(substr(dates[1],1,3),' ',dates[2],', ',year, sep = '')
+    begin <- paste(dates[1],', ',year, sep = '')
+  } else {
+    begin <- paste(dates[1],', ',year, sep = '')
+    if(substr(dates[1],1,3) %like% 'Dec' & substr(dates[2],1,3) %like% 'Jan'){
+      end <- paste(dates[2],', ',as.numeric(year)+1, sep = '')
+    } else {
+      end <- paste(dates[2],', ',year, sep = '')
+    }
+  }
+  begin <- as.Date(begin,'%b %d, %Y')
+  end <- as.Date(end,'%b %d, %Y')
+  
+  days <- as.numeric(end - begin)
+  gross <- gro[i]
+  theaters <- thea[i]
+  
+  df <- NULL
+  df$rel_week <- NA
+  df$week <- NA
+  df$theaters <- NA
+  df$days <- NA
+  df$gross <- NA
+  
+  df <- NULL
+  df$rel_week <- rel_week
+  df$week <- week
+  df$theaters <- theaters
+  df$days <- days
+  df$gross <- gross
+  
+  return(as.data.table(df))
+  
+}
 
